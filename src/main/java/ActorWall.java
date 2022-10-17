92066d31fc44a1602e36a07fa886e19899bbba43
knchan03 <knchan@dons.usfca.edu>
2022-10-17 12:58:04 -0700

    Added javadoc comments

--- src/main/java/ActorWall.java
+++ src/main/java/ActorWall.java
@@ -1,24 +1,31 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 public class ActorWall {
     private String name;
     private List<String> movies = new ArrayList<String>();
     private List<String> characters = new ArrayList<String>();
 
+    /**
+     * Constructor for ActorWall
+     * @param name name of actor
+     */
     public ActorWall(String name){
         this.name = name;
     }
 
+    /**
+     * Adds movie name and character name using same index in separate arraylists
+     * @param movie movie name
+     * @param character character name
+     */
     public void addMovie(String movie, String character){
         movies.add(movie);
         characters.add(character);
     }
 
-    public List<String> getCharacters() {
-        return characters;
-    }
-
+    /**
+     * Prints all movies and characters in those movies
+     */
     public void getMovies(){
         System.out.println("Actor: " + this.name);
         for (int i = 0; i < this.movies.size(); i++){
@@ -26,6 +33,10 @@ public class ActorWall {
         }
     }
 
+    /**
+     *
+     * @return actor name
+     */
     public String getName(){
         return name;
     }
--- src/main/java/project01.java
+++ src/main/java/project01.java
@@ -1,17 +1,24 @@
-import com.opencsv.*;
+/**
+ * Project 01 - MovieWall
+ * @author Kevin Chan
+ * 10/18/2022
+ */
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class project01{
-
+    /**
+     * Creates ActorWall object and adds it to list of ActorWalls
+     * @param actor actor name
+     * @param movie movie name
+     * @param character character name
+     * @param actorsList List of ActorWalls
+     */
     public static void addActor(String actor, String movie, String character, List<ActorWall> actorsList){
 
         for (int i = 0;i < actorsList.size(); i++){
@@ -20,11 +27,17 @@ public class project01{
                 return;
             }
         }
-        ActorWall wall = new ActorWall(actor);
-        wall.addMovie(movie, character);
-        actorsList.add(wall);
+        ActorWall wall = new ActorWall(actor); //creates new ActorWall object
+        wall.addMovie(movie, character); //adds movie and character played
+        actorsList.add(wall); //adds wall to list of ActorWalls
     }
 
+    /**
+     * Quicksorts unsorted list of ActorWalls
+     * @param unsorted_actors unsorted list of ActorWalls
+     * @param left left index
+     * @param right right index
+     */
     public static void quickSort(List<ActorWall> unsorted_actors, int left, int right) {
         if (left < right){
             int p = partition(unsorted_actors, left, right);
@@ -32,12 +45,27 @@ public class project01{
             quickSort(unsorted_actors, p + 1, right);
         }
     }
+
+    /**
+     *
+     * @param actorWall List of ActorWalls
+     * @param i index of first element being switched
+     * @param j index of second element being switched
+     */
     public static void swap(List<ActorWall> actorWall, int i, int j) {
         ActorWall temp = new ActorWall(" ");
         temp = actorWall.get(i);
         actorWall.set(i, actorWall.get(j));
         actorWall.set(j, temp);
     }
+
+    /**
+     * Partition method of quicksort
+     * @param actorWall List of ActorWalls
+     * @param left left boundary
+     * @param right right boundary
+     * @return pivot index
+     */
     public static int partition(List<ActorWall> actorWall, int left, int right) {
         int pivot = right;
 
@@ -52,14 +80,13 @@ public class project01{
         swap(actorWall,i + 1, right);
         return (i + 1);
     }
-    public static void printAllActors(List<ActorWall> actorWall){
-        for (int i = 0; i < actorWall.size(); i++){
-            //if (Character.isUpperCase(actorWall.get(i).getName().charAt(0)) == true)
-            System.out.println(actorWall.get(i).getName());
-        }
-        System.out.println(actorWall.size());
-    }
 
+    /**
+     * Binary searches list of ActorWalls using actor name
+     * @param actorWall List of ActorWalls
+     * @param actor Actor being searched for
+     * @return index of nearest actor if not found, -1 if found
+     */
     public static int search(List<ActorWall> actorWall, String actor){
         int min = 0;
         int max = actorWall.size() - 1;
@@ -67,7 +94,7 @@ public class project01{
         while (min <= max){
             mid = (min + max) / 2;
             if (actorWall.get(mid).getName().compareTo(actor) == 0){
-                actorWall.get(mid).getMovies();
+                actorWall.get(mid).getMovies(); //prints all roles of actor
                 return -1;
             }
             else if (actorWall.get(mid).getName().compareTo(actor) < 0){
@@ -80,6 +107,11 @@ public class project01{
         return mid;
     }
 
+    /**
+     * Reads file and adds actors into a list of ActorWalls
+     * @param stream filepath
+     * @return List of ActorWalls
+     */
     public static List<ActorWall> readFile(String stream){
         List<ActorWall> actorsList = new ArrayList<ActorWall>();
         try{
@@ -89,8 +121,6 @@ public class project01{
             BufferedReader buff_reader = new BufferedReader(file_reader);
 
             String line;
-            int count = 0;
-            int char_count = 0;
             String actor = "";
             String[] line_data;
             String[] movie_data;
@@ -99,24 +129,20 @@ public class project01{
             String character_name = "";
             String movie_name;
             while ((line = buff_reader.readLine()) != null) {
-                if (count != 0) {
-                    line_data = line.split("\\[\\{");
-                    if (line_data.length > 1) {
-                        movie_data = line_data[0].split(",");
-                        movie_name = movie_data[1];
-                        cast_data = line_data[1].split("}, ");
-                        for (int i = 0; i < cast_data.length; i++) {
-                            character_data = cast_data[i].split(", ");
-                            if (character_data[1].contains("character"))
-                                character_name = character_data[1].replace("\"\"character\"\": ", "").replace("\"", "");
-                            if ((character_data[5].contains("name")))
-                                actor = character_data[5].replace("\"\"name\"\": ", "").replace("\"", "");
-                            addActor(actor, movie_name, character_name, actorsList);
-                            char_count++;
-                        }
+                line_data = line.split("\\[\\{"); //separates json from rest of line
+                if (line_data.length > 1) { // checks for header line
+                    movie_data = line_data[0].split(","); // splits movie name and movie id
+                    movie_name = movie_data[1];
+                    cast_data = line_data[1].split("}, "); //splits json text to get all cast members
+                    for (int i = 0; i < cast_data.length; i++) {
+                        character_data = cast_data[i].split(", "); //splits each cast member data to extract character name and actor name
+                        if (character_data[1].contains("character"))
+                            character_name = character_data[1].replace("\"\"character\"\": ", "").replace("\"", ""); //gets character name by itself
+                        if ((character_data[5].contains("name")))
+                            actor = character_data[5].replace("\"\"name\"\": ", "").replace("\"", ""); // gets actor name by itself
+                        addActor(actor, movie_name, character_name, actorsList); //adds actor into actorsList
                     }
                 }
-                count++;
             }
             file_reader.close();
         }
@@ -127,11 +153,10 @@ public class project01{
     }
 
     public static void main(String[] args) {
-        List<ActorWall> actors = readFile("src/main/java/tmdb_5000_credits.csv");
+        List<ActorWall> actors = readFile(args[0]); //path to file stated in configurations (Ex: "src/main/java/tmdb_5000_credits.csv")
         quickSort(actors, 0, actors.size() - 1);
         Scanner sc = new Scanner(System.in);
         String actor = "";
-        printAllActors(actors);
         System.out.println("Welcome to Movie Wall!");
         while (!(actor.equalsIgnoreCase("EXIT"))) {
             System.out.println("Enter the name of an actor (or \"EXIT\" to quit): ");
